85c69463d851f4b63d06312b8f758b54fc64d656
Adnan <adnan.saif.322@gmail.com>
2023-12-26 23:30:36 +0600

    product model

---
+++ src/models/productModel.js
@@ -0,0 +1,77 @@
+const { Schema, model } = require("mongoose");
+
+//name, slug, description, price, quantity, sold, shipping, image
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Product name required"],
+      trim: true,
+      minlegth: [3, "Minimum product name length can be 3"],
+    },
+    slug: {
+      type: String,
+      required: [true, "Product slug required"],
+      lowercase: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Product description required"],
+      trim: true,
+      minlegth: [3, "Minimum product description length can be 3"],
+    },
+    price: {
+      type: number,
+      required: [true, "Product price required"],
+      trim: true,
+      validate: {
+        validator: (v) => v > 0,
+        message: (props) => {
+          `${props.value} is not a valid price! Price must be greater than 0`;
+        },
+      },
+    },
+    quantity: {
+      type: number,
+      required: [true, "Product quantity required"],
+      trim: true,
+      validate: {
+        validator: (v) => v > 0,
+        message: (props) => {
+          `${props.value} is not a valid quantity! quantity must be greater than 0`;
+        },
+      },
+    },
+    sold: {
+      type: number,
+      required: [true, "Product sold required"],
+      trim: true,
+      default: 0,
+      validate: {
+        validator: (v) => v > 0,
+        message: (props) => {
+          `${props.value} is not a valid sold quantity! sold quantity must be greater than 0`;
+        },
+      },
+    },
+    shipping: {
+      type: number,
+      default: 0, //shipping free then 0
+    },
+    image: {
+      type: String,
+      default: defaultImagePath,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
